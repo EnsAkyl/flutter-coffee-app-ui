8d03b065e3edc6b1dfa6184e09a1289dcb5065aa
Enes AKYOL <akyolenes77@gmail.com>
2025-03-15 01:26:12 +0300

    refactor: sayfa adlandırlaması değiştirildi (SignUpPage -> SignInPage)

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({super.key});
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
 
   @override
-  State<SignUpPage> createState() => _SignUpPageState();
+  State<SignInPage> createState() => _SignInPageState();
 }
 
-class _SignUpPageState extends State<SignUpPage> {
+class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
